a4369196df737a5698cf193360d98ef8ef8d020d
John Fleurimond <johnfleurimond@Johns-MacBook-Pro.local>
2022-09-15 12:28:04 -0400

    first commit

---
+++ components/Card/Card.jsx
@@ -0,0 +1,39 @@
+import { Card, YoutubeCardContent, CardBody, AVideo, CardImage } from './index';
+import { useRef } from 'react';
+const getProductCard = (props) => {
+  const [card, flipCard] = useState(false);
+  const cardRef = useRef({});
+
+  return card ? (
+    <>
+      <Card>
+        <CardBody
+          ref={cardRef}
+          disabled={cardRef}
+          onClick={() => flipCard(false)}
+          role='contentInfo'
+          aria-pressed='false'
+          aria-label='Card with Youtube Title, click watch here to view.'
+        >
+          <YoutubeCardContent aria-label='description'>{props.description}</YoutubeCardContent>
+
+          <YoutubeCardContent aria-label='videoOwnerChannelTitle'>{props.videoOwnerChannelTitle}</YoutubeCardContent>
+          <CardImage src={props.url} href={props.href} />
+          <AVideo aria-label='videoOwnerChannelTitle' href={props.videoId}>
+            <b>Watch Here: </b>
+          </AVideo>
+        </CardBody>
+      </Card>
+    </>
+  ) : (
+    <Card key={i} ref={cardRef} onClick={() => flipCard(true)}>
+      <CardBody>
+        <NameFieldset aria-label='title'>{props.title}</NameFieldset>
+        <div role='contentInfo' aria-pressed='true' aria-label='Card with Youtube Title, click watch here to view.'>
+          <CardImage src={props.url} href={`https://www.youtube.com/embed/${props.videoId}`} />
+        </div>
+      </CardBody>
+    </Card>
+  );
+};
+export default getProductCard;
