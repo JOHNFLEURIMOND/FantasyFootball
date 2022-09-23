2b268be6dfc2cc0cdfb20dca6cc4e709ec437dd9
John Fleurimond <johnfleurimond@Johns-MacBook-Pro.local>
2022-09-22 20:25:35 -0400

    Fourth Commit: Working on Pagination..PPR/-> playercards * pagination

--- components/PPR/PPR.jsx
+++ components/PPR/PPR.jsx
@@ -1,14 +1,13 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Nav from '../Navbar/Nav.jsx';
 import Footer from '../Footer/Footer';
 import MainHero from '../MainHero/MainHero';
-import {Provider as StyletronProvider, DebugEngine} from 'styletron-react';
-import {Client as Styletron} from 'styletron-engine-atomic';
-import {Card, CardHeader, YoutubeCardContent, CardBody, NameFieldset, PriceFieldset, AVideo} from '../Card/index';
-import axios from 'axios';
-import {ProjectsSectionContainer, Title, CardDiv} from './index';
-import {GlobalStyle, Container} from '../CSS/global-style';
-import {Icon, Pagination, Dimmer, Loader, Image, Segment} from 'semantic-ui-react';
+import Pagination from './Pagination';
+import PlayerCards from './PlayerCards';
+import { Provider as StyletronProvider, DebugEngine } from 'styletron-react';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { GlobalStyle, Container } from '../CSS/global-style';
 
 const debug = process.env.NODE_ENV === 'production' ? void 0 : new DebugEngine();
 const engine = new Styletron();
@@ -22,91 +21,33 @@ export default function PPR() {
   const [search, setSearch] = useState('');
 
   useEffect(() => {
-    const getStats = async () => {
-      setLoaded(true);
-      await axios
-        .get(`https://api.sportsdata.io/v3/nfl/stats/json/FantasyPlayers?key=${key}`)
-        .then((response) => {
-          setPprStats(response.data), console.log(response.data);
-        })
-        .catch((error) => setError(error.message))
-        .finally(() => setLoaded(false));
-    };
-    getStats();
+      const getStats = async () => {
+          setLoaded(true);
+          const res = await axios
+              .get(`https://api.sportsdata.io/v3/nfl/projections/json/PlayerGameProjectionStatsByWeek/2022REG/3?key=${key}`)
+          setPprStats(res.data)
+          setLoaded(false)
+      };
+      getStats();
   }, []);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = pprStats.slice(indexOfFirstPost, indexOfLastPost);
+  
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostPerPage] = useState(12);
+
+  // Get current posts
+  const paginate = (pageNumbers) => setCurrentPage(pageNumbers)
+
   return (
     <Container>
       <GlobalStyle />
       <Nav />
       <MainHero />
-      <ProjectsSectionContainer>
-        <Title>Fantasy Football PPR Stats</Title>
-        <div>{error.message}</div>
-        <h1>Search Players</h1>
-        <input type='text' placeholder='Search For News' onChange={(e) => setSearch(e.target.value)} />
-        <CardDiv>
-          {loaded ? (
-            <Segment>
-              <Dimmer active inverted>
-                <Loader size='large'>Loading</Loader>
-              </Dimmer>
-
-              <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
-            </Segment>
-          ) : (
-            pprStats
-              .filter((value) => {
-                if (search === '') {
-                  return value;
-                } else if (value.Name.toLowerCase().includes(search.toLowerCase())) {
-                  return value;
-                }
-              })
-              .sort((a, b) => a - b)
-              .map((d, index) => (
-                <div>
-                  {card ? (
-                    <Card key={d.PlayerID}>
-                      <CardBody
-                        onClick={() => flipCard(false)}
-                        role='contentInfo'
-                        aria-pressed='false'
-                        aria-label='Product Card with a Image and a list of price, type of strain, thc and cbd levels.'>
-                        <CardHeader role='img' aria-label='Description of the Product image'>
-                          <NameFieldset aria-label='title'>
-                            Rushing Attempts : {d.Position === 'RB' ? `${d.RushingAttempts}` : 'No Rushes'}
-                          </NameFieldset>
-                        </CardHeader>
-                        <NameFieldset aria-label='title' key={d.PlayerID}>
-                          Rushing Yards: {d.Position === 'RB' ? `${d.RushingYards}` : 'No Rushes'}
-                        </NameFieldset>
-                        <NameFieldset aria-label='title' key={d.PlayerID}>
-                          Rushing TDS: {d.Position === 'RB' ? `${d.RushingTouchdowns}` : 'No Rushes'}
-                        </NameFieldset>
-                        <NameFieldset aria-label='title' key={d.PlayerID}>
-                          Receiving Yards and TDS: {d.ReceivingYardsPerReception} : {d.ReceivingTouchdowns}
-                        </NameFieldset>
-                      </CardBody>
-                    </Card>
-                  ) : (
-                    <Card key={d.PlayerID}>
-                      <CardBody onClick={() => flipCard(true)}>
-                        <CardHeader role='img' aria-label='Description of the overall image'>
-                          <YoutubeCardContent aria-label='title'>
-                            {d.Position === 'RB' ? `${d.Name}` : 'No Rushes'}
-                          </YoutubeCardContent>
-                        </CardHeader>
-                        <AVideo aria-label='description'>
-                          Players Team: {d.Team} VS: {d.Opponent}{' '}
-                        </AVideo>
-                      </CardBody>
-                    </Card>
-                  )}
-                </div>
-              ))
-          )}
-        </CardDiv>
-      </ProjectsSectionContainer>
+      <PlayerCards posts={currentPosts} loaded={loaded} />
+      <Pagination postPerPage={postsPerPage} paginate={paginate} />
       <Footer />
     </Container>
   );
