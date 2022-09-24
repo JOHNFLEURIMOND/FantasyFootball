cb22bd5e40bec9ec6889b47f94e4cf8bf7e30f2c
John Fleurimond <johncfleurimond@gmail.com>
2022-09-23 23:08:45 -0400

    Fifth Commit: Need to finish Pagination..PPR/weeklyprojected

---
+++ components/Pagination/Pagination.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Pagination} from 'semantic-ui-react';
+
+const AppPagination = ({statsPerPage, totalStats, paginate}) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalStats / statsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav>
+      <ul className='pagination'>
+        {pageNumbers.map((number) => (
+          <li key={number} className='page-item'>
+            <a onClick={() => paginate(number)} className='page-link'>
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default AppPagination;
