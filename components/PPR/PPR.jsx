2130c53c0940d1e01a7c8cf525020af84b630182
John Fleurimond <johncfleurimond@gmail.com>
2022-09-23 23:08:45 -0400

    Fifth Commit: Need to finish Pagination..PPR/weeklyprojected

--- components/PPR/PPR.jsx
+++ components/PPR/PPR.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Nav from '../Navbar/Nav.jsx';
 import Footer from '../Footer/Footer';
 import MainHero from '../MainHero/MainHero';
-import Pagination from './Pagination';
+import Pagination from '../Pagination/Pagination';
 import PlayerCards from './PlayerCards';
-import { Provider as StyletronProvider, DebugEngine } from 'styletron-react';
-import { Client as Styletron } from 'styletron-engine-atomic';
-import { GlobalStyle, Container } from '../CSS/global-style';
+import {Provider as StyletronProvider, DebugEngine} from 'styletron-react';
+import {Client as Styletron} from 'styletron-engine-atomic';
+import {GlobalStyle, Container} from '../CSS/global-style';
 
 const debug = process.env.NODE_ENV === 'production' ? void 0 : new DebugEngine();
 const engine = new Styletron();
@@ -15,39 +15,35 @@ const key = process.env.REACT_APP_MY_API_KEY;
 
 export default function PPR() {
   const [pprStats, setPprStats] = useState([]);
-  const [card, flipCard] = useState(false);
-  const [error, setError] = useState('');
   const [loaded, setLoaded] = useState(false);
-  const [search, setSearch] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [statsPerPage] = useState(12);
+
+  const indexOfLastStat = currentPage * statsPerPage;
+  const indexOfFirstStat = indexOfLastStat - statsPerPage;
+  const currentStats = pprStats.slice(indexOfFirstStat, indexOfLastStat);
+  // Get current stats
+  const paginate = (pageNumbers) => setCurrentPage(pageNumbers);
 
   useEffect(() => {
-      const getStats = async () => {
-          setLoaded(true);
-          const res = await axios
-              .get(`https://api.sportsdata.io/v3/nfl/projections/json/PlayerGameProjectionStatsByWeek/2022REG/3?key=${key}`)
-          setPprStats(res.data)
-          setLoaded(false)
-      };
-      getStats();
+    const getStats = async () => {
+      setLoaded(true);
+      const res = await axios.get(
+        `https://api.sportsdata.io/v3/nfl/projections/json/PlayerGameProjectionStatsByWeek/2022REG/3?key=${key}`,
+      );
+      setPprStats(res.data);
+      setLoaded(false);
+    };
+    getStats();
   }, []);
-
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = pprStats.slice(indexOfFirstPost, indexOfLastPost);
-  
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostPerPage] = useState(12);
-
-  // Get current posts
-  const paginate = (pageNumbers) => setCurrentPage(pageNumbers)
 
   return (
     <Container>
       <GlobalStyle />
       <Nav />
       <MainHero />
-      <PlayerCards posts={currentPosts} loaded={loaded} />
-      <Pagination postPerPage={postsPerPage} paginate={paginate} />
+      <PlayerCards stats={currentStats} loaded={loaded} />
+      <Pagination statsPerPage={statsPerPage} paginate={paginate} />
       <Footer />
     </Container>
   );
--- components/WeeklyProjections/WeeklyProjections.jsx
+++ components/WeeklyProjections/WeeklyProjections.jsx
@@ -1,25 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import Nav from '../Navbar/Nav.jsx';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Footer from '../Footer/Footer';
 import MainHero from '../MainHero/MainHero';
-import { Provider as StyletronProvider, DebugEngine } from 'styletron-react';
-import { Client as Styletron } from 'styletron-engine-atomic';
-import { ProjectsSectionContainer, CardDiv, Title } from './index';
-import { Card, CardHeader, YoutubeCardContent, CardBody, NameFieldset, PriceFieldset, AVideo } from '../Card/index';
-import axios from 'axios';
-import { GlobalStyle, Container } from '../CSS/global-style';
-import { Icon, Pagination, Dimmer, Loader, Image, Segment } from 'semantic-ui-react';
+import Nav from '../Navbar/Nav.jsx';
+import Pagination from '../Pagination/Pagination';
+import WeeklyProjectionCards from './WeeklyProjectionCards';
+import {Provider as StyletronProvider, DebugEngine} from 'styletron-react';
+import {Icon} from 'semantic-ui-react';
+import {Client as Styletron} from 'styletron-engine-atomic';
+import {ProjectsSectionContainer, Title} from './index';
+import {GlobalStyle, Container} from '../CSS/global-style';
 const key = process.env.REACT_APP_MY_API_KEY;
 
 const debug = process.env.NODE_ENV === 'production' ? void 0 : new DebugEngine();
 const engine = new Styletron();
 
 function WeeklyProjections() {
-  const [card, flipCard] = useState(false);
   const [stats, setStats] = useState([]);
   const [error, setError] = useState('');
   const [loading, setLoaded] = useState(false);
-  const [search, setSearch] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [statsPerPage] = useState(12);
+  const indexOfLastStat = currentPage * statsPerPage;
+  const indexOfFirstStat = indexOfLastStat - statsPerPage;
+  const currentStats = stats.slice(indexOfFirstStat, indexOfLastStat);
+
+  // Get current stats
+  const paginate = (pageNumbers) => setCurrentPage(pageNumbers);
 
   useEffect(() => {
     setLoaded(true);
@@ -27,7 +34,7 @@ function WeeklyProjections() {
       await axios
         .get(`https://api.sportsdata.io/v3/nfl/projections/json/PlayerGameProjectionStatsByWeek/2022REG/3?key=${key}`)
         .then((responses) => {
-          setStats(responses.data), console.log(responses.data);
+          setStats(responses.data);
         })
         .catch((error) => setError(error))
         .finally(() => setLoaded(false));
@@ -35,6 +42,10 @@ function WeeklyProjections() {
 
     getStats();
   }, []);
+  {
+    'this outside useEffects: ', console.log(stats.length);
+  }
+
   return (
     <Container>
       <GlobalStyle />
@@ -42,83 +53,10 @@ function WeeklyProjections() {
       <MainHero />
       <ProjectsSectionContainer>
         <Title>Fantasy Football News</Title>
-        <h1>Search Players</h1>
-        <input type='text' placeholder='Search For News' onChange={(e) => setSearch(e.target.value)} />
-        {console.log(error)}
-        <CardDiv>
-          {loading ? (
-            <Segment>
-              <Dimmer active inverted>
-                <Loader size='large'>Loading</Loader>
-              </Dimmer>
-
-              <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
-            </Segment>
-          ) : (
-            stats
-              .filter((value) => {
-                if (search === '') {
-                  return value;
-                } else if (value.Name.toLowerCase().includes(search.toLowerCase())) {
-                  return value;
-                }
-              })
-              .sort((a, b) => a - b)
-              .map((d, index) => (
-                <div>
-                  {card ? (
-                    <Card key={d.PlayerID}>
-                      <CardBody
-                        onClick={() => flipCard(false)}
-                        role='contentInfo'
-                        aria-pressed='false'
-                        aria-label='Product Card with a Image and a list of price, type of strain, thc and cbd levels.'>
-                        <CardHeader role='img' aria-label='Description of the Product image'>
-                          <NameFieldset aria-label='title'>
-                            Active: {d.Activated === 1 ? 'Active' : 'Not Active'}
-                          </NameFieldset>
-                        </CardHeader>
-                        <NameFieldset aria-label='title'>Fantasy Points FanDuel: {d.FantasyPointsFanDuel}</NameFieldset>
-                        <NameFieldset aria-label='description'>Fantasy Points: {d.FantasyPoints}</NameFieldset>
-                        <NameFieldset aria-label='description'>FantasyPointsPPR: {d.FantasyPointsPPR}</NameFieldset>
-                      </CardBody>
-                    </Card>
-                  ) : (
-                    <Card key={d.PlayerID}>
-                      <CardBody onClick={() => flipCard(true)}>
-                        <CardHeader role='img' aria-label='Description of the overall image'>
-                          <YoutubeCardContent aria-label='title'>
-                            {d.Name} : {d.Position}
-                          </YoutubeCardContent>
-                        </CardHeader>
-                        <AVideo aria-label='description'>
-                          Players Team: {d.Team} VS: {d.Opponent}{' '}
-                        </AVideo>
-                        <AVideo aria-label='description'>
-                          {d.HomeOrAway === 'AWAY' ? 'Playing Away' : 'Playing At Home'}
-                        </AVideo>
-                        <AVideo aria-label='description'>Game Date: {d.GameDate}</AVideo>
-                      </CardBody>
-                    </Card>
-                  )}
-                </div>
-              ))
-          )}
-        </CardDiv>
-        <Pagination
-          defaultActivePage={5}
-          ellipsisItem={{
-            content: <Icon name='ellipsis horizontal' />,
-            icon: true,
-          }}
-          firstItem={{ content: <Icon name='angle double left' />, icon: true }}
-          lastItem={{ content: <Icon name='angle double right' />, icon: true }}
-          prevItem={{ content: <Icon name='angle left' />, icon: true }}
-          nextItem={{ content: <Icon name='angle right' />, icon: true }}
-          totalPages={10}
-        />
+        <WeeklyProjectionCards stats={currentStats} loading={loading} />
+        <Pagination statsPerPage={statsPerPage} paginate={paginate} totalStats={Math.ceil(stats.length)} />
       </ProjectsSectionContainer>
-      s <Footer />
+      <Footer />
     </Container>
   );
 }
