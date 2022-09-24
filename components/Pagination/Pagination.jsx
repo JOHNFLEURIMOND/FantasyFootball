a6c139d967e6178a74a24e3c9c04421efee6dcb9
John Fleurimond <johncfleurimond@gmail.com>
2022-09-23 23:56:57 -0400

    Fifth Commit: Need to finish Pagination..PPR/weeklyprojected

--- components/Pagination/Pagination.jsx
+++ components/Pagination/Pagination.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
-import { Pagination } from 'semantic-ui-react';
+import {Pagination} from 'semantic-ui-react';
 
-const AppPagination = ({ statsPerPage, totalStats, paginate }) => {
-    const pageNumbers = [];
+const AppPagination = ({statsPerPage, totalStats, paginate}) => {
+  const pageNumbers = [];
 
-    for (let i = 1; i <= Math.ceil(totalStats / statsPerPage); i++) {
-        pageNumbers.push(i);
-    }
+  for (let i = 1; i <= Math.ceil(totalStats / statsPerPage); i++) {
+    pageNumbers.push(i);
+  }
 
-    return (
-        <nav>
-            <ul className='pagination'>
-                {pageNumbers.map((number) => (
-                    <>
-                        <Pagination key={number} onPageChange={() => paginate(number)} defaultActivePage={1} totalPages={number} />
-                        <li key={number} className='page-item'>
-                            <a onClick={() => paginate(number)} className='page-link'>
-                                {number}
-                            </a>
-                        </li>
-                    </>
-                ))}
-            </ul>
-        </nav>
-    );
+  return (
+    <nav>
+      <ul className='pagination'>
+        {pageNumbers.map((number) => (
+          <>
+            <Pagination key={number} onPageChange={() => paginate(number)} defaultActivePage={1} totalPages={number} />
+            <li key={number} className='page-item'>
+              <a onClick={() => paginate(number)} className='page-link'>
+                {number}
+              </a>
+            </li>
+          </>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default AppPagination;
