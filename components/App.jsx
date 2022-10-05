abb16f67dde59fd442b0ea5cc64a427cd4ec5d33
johnfleurimond <johncfleurimond@gmail.com>
2022-10-05 19:05:14 -0400

    fix pagination

--- components/App.jsx
+++ components/App.jsx
@@ -1,45 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import Nav from '../components/Navbar/Nav.jsx';
 import Footer from '../components/Footer/Footer';
 import MainHero from '../components/MainHero/MainHero';
 import FantasyFootballRanking from '../components/FantasyFootballRanking/FantasyFootballRanking';
 import WeeklyProjections from '../components/WeeklyProjections/WeeklyProjections';
 import PPR from '../components/PPR/PPR';
-import { Provider as StyletronProvider, DebugEngine } from 'styletron-react';
-import { Client as Styletron } from 'styletron-engine-atomic';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import {Provider as StyletronProvider, DebugEngine} from 'styletron-react';
+import {Client as Styletron} from 'styletron-engine-atomic';
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
 
 const debug = process.env.NODE_ENV === 'production' ? void 0 : new DebugEngine();
 const engine = new Styletron();
 
-import { GlobalStyle, Container } from '../components/CSS/global-style';
+import {GlobalStyle, Container} from '../components/CSS/global-style';
 
 const Homepage = (props) => {
   return (
-        <Container>
-          <GlobalStyle />
-          <Nav />
-          <MainHero />
-          <FantasyFootballRanking />
-          <Footer />
-        </Container>
+    <Container>
+      <GlobalStyle />
+      <Nav />
+      <MainHero />
+      <FantasyFootballRanking />
+      <Footer />
+    </Container>
   );
 };
 
 const App = () => (
-    <StyletronProvider value={engine} debug={debug} debugAfterHydration>
-      <Router location={history.location} navigator={history}>
-        <Switch>
-          <Route path='/' exact component={Homepage} />
-        </Switch>
-        <Switch>
-          <Route path='/WeeklyProjections' component={WeeklyProjections} />
-        </Switch>
-        <Switch>
-          <Route path='/PPR' component={PPR} />
-        </Switch>
-      </Router>
-    </StyletronProvider>
+  <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+    <Router location={history.location} navigator={history}>
+      <Switch>
+        <Route path='/' exact component={Homepage} />
+      </Switch>
+      <Switch>
+        <Route path='/WeeklyProjections' component={WeeklyProjections} />
+      </Switch>
+      <Switch>
+        <Route path='/PPR' component={PPR} />
+      </Switch>
+    </Router>
+  </StyletronProvider>
 );
 
 export default App;
--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,21 +1,20 @@
-import React from "react";
-import { Div, Icon } from "atomize";
+import React from 'react';
+import {Div, Icon} from 'atomize';
 
 const Loading = () => {
   return (
     <Div
-      bg="transparent"
-      d="flex"
-      align="center"
-      justify="center"
-      pos="absolute"
-      top="0"
-      bottom="0"
-      right="0"
-      left="0"
-      style={{ zIndex: -1 }}
-    >
-      <Icon name="Loading3" size="4rem" color="brand500" />
+      bg='transparent'
+      d='flex'
+      align='center'
+      justify='center'
+      pos='absolute'
+      top='0'
+      bottom='0'
+      right='0'
+      left='0'
+      style={{zIndex: -1}}>
+      <Icon name='Loading3' size='4rem' color='brand500' />
     </Div>
   );
 };
