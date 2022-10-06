aa488d56c433a7bd229e2d800fa33b2b94769b87
johnfleurimond <johncfleurimond@gmail.com>
2022-10-06 15:56:37 -0400

    PPR Page completed; Fix all cards flipping. instead of one click

--- components/WeeklyProjections/WeeklyProjections.jsx
+++ components/WeeklyProjections/WeeklyProjections.jsx
@@ -1,37 +1,15 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, {useContext} from 'react';
 import Footer from '../Footer/Footer';
 import MainHero from '../MainHero/MainHero';
 import Nav from '../Navbar/Nav.jsx';
 import Pagination from '../Pagination/Pagination';
 import WeeklyProjectionCards from './WeeklyProjectionCards';
 import {ProjectsSectionContainer, Title} from './index';
+import {StatsContext} from '../App';
 import {GlobalStyle, Container} from '../CSS/global-style';
-const key = process.env.REACT_APP_MY_API_KEY;
 
 function WeeklyProjections() {
-  const [stats, setStats] = useState([]);
-  const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const indexOfLastStat = currentPage * 12;
-  const totalPages = stats.length / 12;
-  const indexOfFirstStat = indexOfLastStat - 12;
-  const currentStats = stats.slice(indexOfFirstStat, indexOfLastStat);
-
-  useEffect(() => {
-    setLoading(true);
-    const getStats = async () => {
-      await axios
-        .get(`https://api.sportsdata.io/v3/nfl/projections/json/PlayerGameProjectionStatsByWeek/2022REG/3?key=${key}`)
-        .then((responses) => {
-          setStats(responses.data);
-        })
-        .catch((responses) => setError(responses.error))
-        .finally(() => setLoading(false));
-    };
-    getStats();
-  }, [currentPage]);
+  const {currentPage, currentStats, setCurrentPage, loading, error, totalPages} = useContext(StatsContext);
 
   return (
     <Container>
