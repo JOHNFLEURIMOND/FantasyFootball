868386f7cd20d8ca93bdd884106de03efc22c60c
johnfleurimond <johncfleurimond@gmail.com>
2022-10-08 19:06:32 -0400

    Fin; Deployed.

--- components/Navbar/Nav.jsx
+++ components/Navbar/Nav.jsx
@@ -5,10 +5,10 @@ const Navbar = () => {
   const [open, setOpen] = React.useState(false);
   const node = React.useRef();
   return (
-        <div ref={node}>
-        <Burger open={open} setOpen={setOpen} />
-        <Menu open={open} setOpen={setOpen} />
-      </div>
+    <div ref={node}>
+      <Burger open={open} setOpen={setOpen} />
+      <Menu open={open} setOpen={setOpen} />
+    </div>
   );
 };
 
