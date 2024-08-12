f4d776bf228091e94ca55d1037ce8c54e7c151e7
John Fleurimond <johnfleurimond@Johns-MacBook-Pro.local>
2024-08-11 21:05:35 -0400

    fix news/ home page

--- components/App.test.js
+++ components/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// App.test.jsx
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { render, screen, waitFor } from '@testing-library/react';
+import App from './App';
+import { ThemeProvider } from 'styled-components';
+import theme from './CSS/theme'; // Adjust path as necessary
+
+test('renders learn react link', async () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  );
+
+  // Wait for the main content to be loaded
+  await waitFor(() => {
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
